172d391154e82c37f8722f81ec4fa25fd294d544
TomiukQT <TomiukQT@gmail.com>
2018-08-06 09:30:59 +0200

    Started adding tools

--- UntitledGame1/Assets/Scripts/PlayerController.cs
+++ UntitledGame1/Assets/Scripts/PlayerController.cs
@@ -10,27 +10,47 @@ public class PlayerController : MonoBehaviour {
     private GameObject hand;
     private GameObject camera;
 
+    [SerializeField]
+    private GameObject skillTree;
+
+    public GameObject tools;
+
+
     private GameObject grabbed;
     private bool isGrabbing = false;
+
+    private bool hasTool = false;
+    private GameObject tool;
+
 
     void Start()
     {
         inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
         camera = GameObject.Find("Main Camera");
         hand = GameObject.Find("Hand");
+
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
 
     void Update()
     {
         GetInput();
-        if(isGrabbing)
+        if (isGrabbing)
         {
             Carry(hand);
         }
-        if(isGrabbing && hand.transform.childCount == 0)
+        if (isGrabbing && hand.transform.childCount == 0)
         {
             isGrabbing = false;
+        }
+        if (hasTool)
+        {
+            Carry(tool);
+            //tool.transform.rotation = new Quaternion(camera.transform.rotation.x + 90, camera.transform.rotation.y, camera.transform.rotation.z,1f);
+            
         }
     }
 
@@ -51,7 +71,53 @@ public class PlayerController : MonoBehaviour {
             DropItem();
         }
 
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            Cursor.visible = !Cursor.visible;
+            skillTree.SetActive(!skillTree.activeInHierarchy);
+        }
 
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchTool(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchTool(1);
+        }
+
+    }
+
+    public void SwitchTool(int index)
+    {
+        if (index == 0)
+        {
+            DisableAllTools();
+        }
+        else if(index <= tools.transform.childCount)
+        {
+            hasTool = true;
+            tool = tools.transform.GetChild(index - 1).gameObject;
+            tools.transform.GetChild(index-1).gameObject.SetActive(true);
+        }
+    }
+
+
+    private void DisableAllTools()
+    {
+        for (int i = 0; i < tools.transform.childCount; i++)
+        {
+            tools.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        hasTool = false;
     }
 
     private void GrabItem()
